e0be9f587c781e1efc435a27edc82ba45a61eaf5
Mirjaxon <mirjaxon457@gmail.com>
2024-03-23 18:27:45 +0500

    beg

---
+++ begner.php
@@ -0,0 +1,97 @@
+<?php
+// birinch masala 
+    //$a=4;
+    //$P = $a*4;
+    //echo $P
+//?>
+<?php 
+// ikkinch misol   
+    /*$a=20;
+    $S = pow($a,2);
+    echo $S;
+    */
+?>
+<?php
+// uchunch misol 
+   /* $a = 20;
+    $b = 10;
+    $P = 2*($a+$b);
+    
+    $S = $a*$b;
+    echo  $P;
+    echo $S;
+    */
+// turtinch  misol
+    /*$pi =3.1415;
+    $d  = 30;
+    $L = $pi*$d;
+    echo $L
+    */
+// beshinch misol 
+    /* 
+    $a = 9;
+    
+    $V = pow($a,3);
+    $S = pow($a,2)*6;
+    
+    echo $V;
+    echo $S;
+    */
+// oltinch mislo
+/*
+    $a = 10;
+    $b = 20;
+    $c =30;
+
+    $V = $a*$b*$c;
+
+    $S = 2*($a*$b + $b*$c + $a*$c);
+
+    echo $V;
+    echo $S;
+    */
+// yitttinch misol 
+    /*$pi = 3.1415;
+    $r = 23;
+    $L = 2*$pi*$r;
+    
+    $S = $pi*pow($r,2);
+
+    echo $L;
+    
+    echo $S;
+*/
+
+// sakkizinch misol
+  /*
+    $a = 20;
+    $b = 10;
+
+    $s = ($a+$b)/2;
+    echo $s;
+    */
+// tuqqizinch masala
+/*
+    $a =20;
+    $b = 20;
+    $s = sqrt($a*$b);
+
+    echo $s;
+    */
+// unchi misol 
+
+    $a = 20;
+    $b = 12;
+    $x = $a + $b;
+
+    $y = $a*$b;
+
+    $a1 = pow($a,2);
+    $b2 = pow($b,2);
+
+    echo $x;
+    echo $y;
+    echo $a1;
+    echo $b2;
+
+?>  
